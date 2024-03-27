f85157f8e9ff8d142f217bcbb897b6058bbac2ba
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-03-27 19:55:20 +0530

    created utility app using redux-toolkit

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState={
+    todos:[
+        {text:"Go to Gym at 6", completed: false},
+        {text: "Study at 8", completed: true}
+    ]
+}
+
+//creating reducer using Redux Toolkit
+    const todoSlice = createSlice({
+        name:'todo',
+        initialState:initialState,     
+        reducers:{
+            //this is add action
+           add:(state,action)=>{
+                state.todos.push({
+                    text:action.payload,
+                    completed:false
+                })
+           },
+           toggle:(state,action)=>{
+            state.todos.map((todo, i)=>{
+                if(i === action.payload){
+                    todo.completed =!todo.completed;
+                }
+                return todo;
+            })
+           } 
+
+        }
+    });
+
+    export const todoReducer = todoSlice.reducer;
+    export const actions = todoSlice.actions;
+
+    //selector 
+    export const todoSelector = (state)=> state.todoReducer.todos;
+
+// using React-Redux
+// export function todoReducer(state=initialState, action){
+
+//     switch(action.type){
+//         case ADD_TODO:
+//             return {
+//                 ...state,
+//                 todos:[
+//                     ...state.todos,
+//                     {
+//                         text:action.text,
+//                         completed: false
+//                     }
+//                 ]
+//             }
+//         case TOGGLE_TODO:
+//             return{
+//                 ...state,
+//                 todos: state.todos.map((todo, i)=>{
+//                     if(i==action.index){
+//                         todo.completed=!todo.completed
+//                     }
+//                     return todo;
+//                 })
+//             }
+//         default:
+//             return state;
+//     }
+// }
